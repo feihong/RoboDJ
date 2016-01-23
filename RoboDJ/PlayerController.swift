e66b285428a635acef8967d807439d90e78f9242
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-22 20:02:23 -0600

    Make the layout a bit more balanced

--- RoboDJ/PlayerController.swift
+++ RoboDJ/PlayerController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 
 class PlayerController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var artistLabel: UILabel!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var prevButton: UIButton!
+    @IBOutlet var nextButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
